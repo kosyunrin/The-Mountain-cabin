876e35bcee60cd5013111eb7e76918eebefa8fc7
NoneName <kosyunrin@gmail.com>
2020-07-01 04:18:22 +0900

    S2

---
+++ MCBMGAME/Assets/skFreeEnvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Invector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace SK
+{
+    [vClassHeader("Free Envents ", false, iconName = "icon_ko")]
+    public class skFreeEnvents : vMonoBehaviour
+    {
+        [SerializeField] Animator MonoActionEvents=null;
+        public UnityEvent mFreeEvents=null;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
+        public void FreeEventsInvoke()
+        {
+            mFreeEvents.Invoke();
+        }
+        public void OpenleftDoor()
+        {
+            MonoActionEvents.PlayInFixedTime("CarOpenDoor");
+        }
+        public void CloseleftDoor()
+        {
+            MonoActionEvents.PlayInFixedTime("CarCloseDoor");
+        }
+    }
+}
